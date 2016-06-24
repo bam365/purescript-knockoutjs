309445bf56c0aada2a6863d78f543edcb7334a20
Blake Mitchell <bmitchell@ntst.com>
2016-06-24 11:17:00 -0500

    v0.1.0

--- src/Knockout/Observable.js
+++ src/Knockout/Observable.js
@@ -1,3 +1,43 @@
-'use strict';
+"use strict"; 
 
-var ko = require('knockout');
+// module Knockout.Observable
+
+exports.empty = function() {
+    return ko.observable();
+};
+
+exports.singleton = function(v) {
+    return ko.observable(v);
+};
+
+exports.emptyArray = function() {
+    return ko.observableArray([]);
+};
+
+exports.fromArray = function(arry) {
+    return ko.observableArray(arry);
+};
+
+exports.computed = function(f) {
+    return ko.pureComputed(f);
+};
+
+exports.readUndef = function(o) {
+    return o();
+};
+
+exports.readArray = function(arry) {
+    return arry();
+};
+
+exports.write = function(o) { 
+    return function(v) {
+        o(v);
+    };
+};
+
+exports.writeArray = function(o) {
+    return function(a) {
+        o(a);
+    };
+};
--- src/Knockout/Observable.purs
+++ src/Knockout/Observable.purs
@@ -1,18 +1,53 @@
 module Knockout.Observable where
+    
+import Prelude  
+import Control.Monad.Eff (Eff)
+import Data.Foreign.Undefined (Undefined, unUndefined)
+import Data.Maybe (Maybe)
 
 foreign import data OBSERVABLE :: !
 
+foreign import data OBSERVABLE_RD :: !
+
 foreign import data Observable :: * -> *
+
+{- TODO: I may not need the array type at all. It looks like ko.observableArray() returns the same type as
+   ko.observable() does
+-}
 
 foreign import data ObservableArray :: * -> *
 
 foreign import data Computed :: * -> *
 
-foreign import empty :: forall eff. Eff (observable :: OBSERVABLE | eff) (Observable a)
+foreign import empty :: forall a eff. Eff (observable :: OBSERVABLE | eff) (Observable a)
 
 foreign import singleton :: forall a eff. a -> Eff (observable :: OBSERVABLE | eff) (Observable a)
 
-foreign import emptyArray :: forall eff. Eff (observable :: OBSERVABLE | eff) (ObservableArray a)
+foreign import emptyArray :: forall a eff. Eff (observable :: OBSERVABLE | eff) (ObservableArray a)
 
 foreign import fromArray :: forall a eff. Array a -> Eff (observable :: OBSERVABLE | eff) (ObservableArray a)
 
+foreign import computed :: forall a eff. (Unit -> Eff (observableRd :: OBSERVABLE_RD | eff ) a) 
+                                      -> Eff (observable :: OBSERVABLE | eff) (Computed a)
+
+foreign import readUndef :: forall a eff. Observable a 
+                                       -> Eff (observableRd :: OBSERVABLE_RD | eff) (Undefined a)
+
+read :: forall a eff. Observable a -> Eff (observableRd :: OBSERVABLE_RD | eff) (Maybe a)
+read o = do
+    v <- readUndef o
+    pure $ unUndefined v
+
+foreign import readArray :: forall a eff. ObservableArray a 
+                                       -> Eff (observableRd :: OBSERVABLE_RD | eff) (Array a)
+
+foreign import write :: forall a eff. Observable a -> a -> Eff (observable :: OBSERVABLE | eff) Unit
+
+foreign import writeArray :: forall a eff. ObservableArray a -> Array a 
+                                        -> Eff (observable :: OBSERVABLE | eff) Unit
+
+modify :: forall a eff. (Maybe a -> a) -> Observable a 
+                     -> Eff (observableRd :: OBSERVABLE_RD, observable :: OBSERVABLE | eff) Unit 
+modify f o = do
+    v <- read o
+    write o $ f v
